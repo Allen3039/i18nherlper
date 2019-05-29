2821c8c0d96bc5a62234ef3878fc246f7e284620
wk <weikai.zero@bytedance.com>
2019-05-29 21:06:00 +0800

    somechanges

--- README.md
+++ README.md
@@ -18,14 +18,7 @@ If you have any requirements or dependencies, add a section describing those and
 
 ## Extension Settings
 
-Include if your extension adds any VS Code settings through the `contributes.configuration` extension point.
-
-For example:
-
-This extension contributes the following settings:
-
-* `myExtension.enable`: enable/disable this extension
-* `myExtension.thing`: set to `blah` to do something
+- `18n.localePath`: 国际化匹配文件地址
 
 ## Known Issues
 
@@ -46,20 +39,3 @@ Fixed issue #.
 ### 1.1.0
 
 Added features X, Y, and Z.
-
------------------------------------------------------------------------------------------------------------
-
-## Working with Markdown
-
-**Note:** You can author your README using Visual Studio Code.  Here are some useful editor keyboard shortcuts:
-
-* Split the editor (`Cmd+\` on macOS or `Ctrl+\` on Windows and Linux)
-* Toggle preview (`Shift+CMD+V` on macOS or `Shift+Ctrl+V` on Windows and Linux)
-* Press `Ctrl+Space` (Windows, Linux) or `Cmd+Space` (macOS) to see a list of Markdown snippets
-
-### For more information
-
-* [Visual Studio Code's Markdown Support](http://code.visualstudio.com/docs/languages/markdown)
-* [Markdown Syntax Reference](https://help.github.com/articles/markdown-basics/)
-
-**Enjoy!**
--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,8 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
+import { fstat } from "fs";
+import { resolve } from "url";
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -13,23 +15,23 @@ export function activate(context: vscode.ExtensionContext) {
   // Now provide the implementation of the command with registerCommand
   // The commandId parameter must match the command field in package.json
   let disposable = vscode.commands.registerCommand(
-    'extension.helloWorld',
+    "extension.helloWorld",
     () => {
       // The code you place here will be executed every time your command is executed
 
       // Display a message box to the user
-      vscode.window.showInformationMessage('Hello World1111!');
-    }
+      vscode.window.showInformationMessage("Hello World1111!");
+    },
   );
-  let nima = vscode.commands.registerCommand('nima', () => {
-    vscode.window.showInputBox({}).then((value) => {
-      if (typeof value === 'string') {
+  let nima = vscode.commands.registerCommand("nima", () => {
+    vscode.window.showInputBox({}).then(value => {
+      if (typeof value === "string") {
         vscode.window.showInformationMessage(value);
       }
     });
   });
 
-  let i18nHelper = vscode.commands.registerCommand('i18n', () => {
+  let i18nHelper = vscode.commands.registerCommand("i18n", () => {
     const editor = vscode.window.activeTextEditor;
     if (!editor) {
       return;
@@ -37,17 +39,53 @@ export function activate(context: vscode.ExtensionContext) {
     const tabSize = editor.options.tabSize;
     const document = editor.document;
     const selection = editor.selection;
-    const selectedVar = document.getText(selection);
-    const lineOfSelectedVar = selection.active.line;
-    const config = vscode.workspace.getConfiguration('18n');
+    const key = document.getText(selection);
+
+    const config = vscode.workspace.getConfiguration("18n");
+    const filePath = resolve(
+      (vscode.workspace.rootPath as string) + "/" + vscode.workspace.name,
+      config["localePath"],
+    );
+
+    const file = require(filePath);
+
+    console.log(Object.keys(file));
+    const matchedConfig = findMatched18n(file, key);
+    // const file=require(
+    if (!matchedConfig) {
+      vscode.window.showInformationMessage("没有匹配的key");
+    } else {
+    }
     vscode.window.showInformationMessage(
-      `${selectedVar} ${lineOfSelectedVar} ${JSON.stringify(config)}`
+      JSON.stringify(matchedConfig, null, "\t"),
     );
   });
+
   context.subscriptions.push(disposable);
   context.subscriptions.push(nima);
   context.subscriptions.push(i18nHelper);
 }
 
+interface I18nObj {
+  "en-US": string;
+  "hi-IN": string;
+  "ja-JP": string;
+  "zh-CN": string;
+}
+
+type T18n = {
+  [keyName: string]: {
+    "en-US": string;
+    "hi-IN": string;
+    "ja-JP": string;
+    "zh-CN": string;
+  };
+};
+const findMatched18n = (i1n8Config: T18n, key: string) => {
+  if (typeof i1n8Config[key] !== undefined) {
+    return i1n8Config[key] as I18nObj;
+  }
+  return null;
+};
 // this method is called when your extension is deactivated
 export function deactivate() {}
